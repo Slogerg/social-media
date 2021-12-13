b652a1cb2898b318ff5607e0ee348ca967914f62
Slogerg <antonslogerg@gmail.com>
2021-12-13 18:45:18 +0200

    cleaning

--- database/migrations/2021_12_10_135231_create_users_related_table.php
+++ database/migrations/2021_12_10_135231_create_users_related_table.php
@@ -18,9 +18,6 @@ class CreateUsersRelatedTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->integer('related_id')->unsigned();
             $table->timestamps();
-
-//            $table->foreign('user_id')->references('id')->on('users');
-//            $table->foreign('related_id')->references('id')->on('users');
         });
     }
 
